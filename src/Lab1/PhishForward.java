16cecabc2247b8811dfb15688a95e3d2c904a984
Fitz <f18846188605@gmail.com>
2018-11-02 14:33:39 +0800

    improve the project structure and accomplish the phish web

---
+++ src/Lab1/PhishForward.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.net.Socket;
+
+public class PhishForward implements ProxyForward {
+    @Override
+    public void ProxyForward(Socket client, Socket server, long MaxWaitTime) {
+        File PhishFile = new File("./src/Lab1/phish.html");
+        ByteArrayOutputStream PhishCache = new ByteArrayOutputStream();
+        try {
+            if (server != null && !server.isClosed())
+                server.close();
+            Client.CloneStream(PhishCache, new FileInputStream(PhishFile));
+            String PhishConent = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\nConnection: close\r\n\r\n" + PhishCache.toString("UTF-8");
+            client.getOutputStream().write(PhishConent.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
